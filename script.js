5e311a3168d595b88ff47b4e8ee0c9b53d1cffb2
somnif <teewaard@gmail.com>
2024-05-23 16:12:02 +1200

    Added calculator functinality, and a second screen line to track changes

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const add = () => firstDigit + secondDigit;
-const multiply = () => firstDigit * secondDigit
+const add = () =>      parseFloat(firstDigit) + parseFloat(secondDigit);
+const multiply = () => parseFloat(firstDigit) * parseFloat(secondDigit)
 //need to add divide by zero math
-const divide = () => firstDigit / secondDigit
-const minus = () => firstDigit - secondDigit
+const divide = () =>   parseFloat(firstDigit) / parseFloat(secondDigit)
+const subtract = () =>    parseFloat(firstDigit) - parseFloat(secondDigit)
 
 let firstDigit = "0";
 let secondDigit = "";
@@ -10,12 +10,16 @@ let operatorSelected = "";
 
 const operate = () => {
     if (firstDigit && secondDigit && operatorSelected) {
+        console.log (add().toString(), operatorSelected)
         switch (operatorSelected) {
-            case "+": return add()
-            case "x": return multiply()
-            case "/": return divide()
-            case "-": return minus()
+            case "add": firstDigit = add().toString(); break;
+            case "multiply": firstDigit = multiply().toString(); break;
+            case "divide": firstDigit = divide().toString(); break;
+            case "subtract": firstDigit = subtract().toString(); break;
         }
+        secondDigit = ""
+        operatorSelected = ""
+        draw()
     }
 }
 
@@ -30,7 +34,18 @@ const parseDigit = (digit) => {
     } else {
         console.log("Not a digit")
     }
+    console.log(firstDigit, secondDigit, operatorSelected)    
     draw();
+}
+
+const parseOperator = (operator) => {
+        if (!secondDigit) {
+            operatorSelected = operator
+        } else {
+            operate();
+            operatorSelected = operator;
+        }
+        draw(); 
 }
 
 const addToFirst = (digit) => {
@@ -39,21 +54,28 @@ const addToFirst = (digit) => {
     } else {
         firstDigit = firstDigit.concat(digit)
     }
-    console.log("First digit ", firstDigit)
 }
 
 const addToSecond = (digit) => {
     if (secondDigit === 0 || secondDigit === "") {
         secondDigit = digit;
     } else {
-        secondDigit.concat(digit)
+        secondDigit = secondDigit.concat(digit)
     }
-    console.log(secondDigit)
 }
 
+const operatorSymbol = () => {
+    switch (operatorSelected) {
+        case "add": return "+"; break;
+        case "multiply": return "X"; break;
+        case "divide": return "÷"; break;
+        case "subtract": return "-"; break;
+        default: return "";
+}}
 
 const draw = () => {
-    calcScreen.innerText = !operatorSelected ? firstDigit : secondDigit;
+    calcScreen.innerText = !operatorSelected ? firstDigit : secondDigit ? secondDigit : firstDigit;
+    calcScreenTop.innerText = firstDigit + " " + operatorSymbol() + " " + secondDigit
 }
 
 
@@ -63,7 +85,17 @@ calcDigits.forEach(function (digit) {
     digit.addEventListener("click", () => parseDigit(digit.innerText))
 });
 
-const calcOperators = document.querySelectorAll(".calc-operators > div")
-calcOperators.forEach(operator => operator.addEventListener("click", () => console.log(operator.innerText)));
+const addButton = document.querySelector("#add")
+const subtractButton = document.querySelector("#subtract")
+const divideButton = document.querySelector("#divide")
+const multiplyButton = document.querySelector("#multiply")
+const equalsButton = document.querySelector("#equals")
 
-const calcScreen = document.querySelector("#screen > span")
+addButton.addEventListener("click", () => parseOperator("add"))
+subtractButton.addEventListener("click", () => parseOperator("subtract"))
+divideButton.addEventListener("click", () => parseOperator("divide"))
+multiplyButton.addEventListener("click", () => parseOperator("multiply"))
+equalsButton.addEventListener("click", () => operate())
+
+const calcScreen = document.querySelector("#screen > .bottom-row")
+const calcScreenTop = document.querySelector("#screen > .top-row")
